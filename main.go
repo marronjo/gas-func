19dc29bc821c9ed509d7427d1a634c5ad913bc40
Joe Marron <marronjo@tcd.ie>
2024-04-07 11:33:04 +0100

    add go routine, performance comparison

--- main.go
+++ main.go
@@ -4,11 +4,36 @@ import (
 	"encoding/hex"
 	"fmt"
 	"golang.org/x/crypto/sha3"
+	"sync"
+	"time"
 )
 
 func main() {
 	funcSignature := "transfer%d(address,uint256)"
-	tryBruteForce(funcSignature, 100000)
+
+	startSeq := time.Now()
+	tryBruteForce(funcSignature, 1000000)
+	fmt.Println("Sequential brute force done : %s", time.Since(startSeq))
+
+	startThreaded := time.Now()
+	var wg sync.WaitGroup
+	tryBruteForceThreaded(&wg, funcSignature, 1000000)
+	wg.Wait()
+	fmt.Println("Threaded brute force done : %s", time.Since(startThreaded))
+}
+
+func tryBruteForceThreaded(wg *sync.WaitGroup, funcPattern string, tries int) {
+	for i := 0; i < tries; i++ {
+		wg.Add(1)
+		go func(index int) {
+			defer wg.Done()
+			newFuncSig := fmt.Sprintf(funcPattern, index)
+			funcSelector := getFuncSelector(newFuncSig)
+			if funcSelector[0:4] == "0000" {
+				println(newFuncSig + " " + funcSelector)
+			}
+		}(i)
+	}
 }
 
 func tryBruteForce(funcPattern string, tries int) {
