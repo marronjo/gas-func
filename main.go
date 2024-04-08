bc5101c3b0ea1744ac1020f47a8efdcba0424ac4
Joe Marron <marronjo@tcd.ie>
2024-04-08 14:37:56 +0100

    back to basics : benchmark sequential run

--- main.go
+++ main.go
@@ -4,52 +4,49 @@ import (
 	"encoding/hex"
 	"fmt"
 	"golang.org/x/crypto/sha3"
-	"sync"
 	"time"
 )
 
 func main() {
-	funcSignature := "transfer%d(address,uint256)"
-	tries := 100000000
+	funcSignature := "withdraw%d()"
+	tries := 100000
 
-	startSeq := time.Now()
-	tryBruteForce(funcSignature, tries)
-	fmt.Println("Sequential brute force done : %s", time.Since(startSeq))
+	maxZeroes := 0
+	minFuncSelector := ""
+	minFuncName := ""
 
-	startThreaded := time.Now()
-	var wg sync.WaitGroup
-	tryBruteForceThreaded(&wg, funcSignature, tries)
-	wg.Wait()
-	fmt.Println("Threaded brute force done : %s", time.Since(startThreaded))
-}
+	start := time.Now()
 
-func tryBruteForceThreaded(wg *sync.WaitGroup, funcPattern string, tries int) {
 	for i := 0; i < tries; i++ {
-		wg.Add(1)
-		go func(index int) {
-			defer wg.Done()
-			checkZeros(funcPattern, index)
-		}(i)
+		newFuncSig := fmt.Sprintf(funcSignature, i)
+		funcSelector := getFuncSelector(newFuncSig)
+
+		numZeroes := countLeadingZeros(funcSelector)
+		if numZeroes > maxZeroes {
+			maxZeroes = numZeroes
+			minFuncSelector = funcSelector
+			minFuncName = newFuncSig
+		}
 	}
+	fmt.Println("Min Selector\t: ", minFuncSelector)
+	fmt.Println("Min Func Name \t: ", minFuncName)
+	fmt.Println(fmt.Sprintf("Complete in %s with %d tries", time.Since(start), tries))
 }
 
-func tryBruteForce(funcPattern string, tries int) {
-	for i := 0; i < tries; i++ {
-		checkZeros(funcPattern, i)
+func countLeadingZeros(funcSelector string) int {
+	count := 0
+	for _, a := range funcSelector {
+		if a == '0' {
+			count++
+		} else {
+			break
+		}
 	}
-}
-
-func checkZeros(funcPattern string, index int) {
-	newFuncSig := fmt.Sprintf(funcPattern, index)
-	funcSelector := getFuncSelector(newFuncSig)
-	if funcSelector[0:4] == "0000" {
-		println(newFuncSig + " " + funcSelector)
-	}
+	return count
 }
 
 func getFuncSelector(funcSignature string) string {
 	hash := sha3.NewLegacyKeccak256()
 	hash.Write([]byte(funcSignature))
-	funcSelBytes := hash.Sum(nil)
-	return hex.EncodeToString(funcSelBytes[:4])
+	return hex.EncodeToString(hash.Sum(nil)[:4])
 }
