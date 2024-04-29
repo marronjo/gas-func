8286880bfd87160fd7413e7a0612f28581daa395
Joe Marron <marronjo@tcd.ie>
2024-04-29 21:53:03 +0100

    add interactive view, allow user input for gas golf

--- main.go
+++ main.go
@@ -4,21 +4,25 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"strings"
 	"time"
 
 	"github.com/charmbracelet/bubbles/spinner"
+	"github.com/charmbracelet/bubbles/textinput"
 	tea "github.com/charmbracelet/bubbletea"
 
 	"github.com/marronjo/yoke/golf"
 )
 
 type model struct {
-	choices    []string
-	cursor     int
-	selected   map[int]struct{}
-	message    string
-	processing bool
-	spinner    spinner.Model
+	title       string
+	choices     []string
+	cursor      int
+	message     string
+	processing  bool
+	spinner     spinner.Model
+	input       textinput.Model
+	interactive bool
 }
 
 type printMessage string
@@ -31,12 +35,13 @@ type gasGolfResult struct {
 
 func initialModel() model {
 	return model{
+		title: "yoke CLI",
 		choices: []string{
 			"go gas golfing",
 			"print message",
 		},
-		selected: make(map[int]struct{}),
-		spinner:  spinner.New(),
+		spinner: spinner.New(),
+		input:   textinput.New(),
 	}
 }
 
@@ -45,6 +50,8 @@ func (m model) Init() tea.Cmd {
 }
 
 func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	var cmd tea.Cmd
+
 	switch msg := msg.(type) {
 	case tea.KeyMsg:
 		switch msg.String() {
@@ -60,17 +67,17 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				m.cursor++
 			}
 		case "enter", " ":
-			_, ok := m.selected[m.cursor]
-			if ok {
-				delete(m.selected, m.cursor)
-			} else {
-				m.selected[m.cursor] = struct{}{}
+			if m.interactive {
 				m.processing = true
-				if m.cursor == 0 {
-					return m, tea.Batch(gasGolf("mint%d(uint256,address)"), m.spinner.Tick)
-				} else {
-					return m, tea.Batch(createMessage("Print Message", 2), m.spinner.Tick)
-				}
+				return m, tea.Batch(gasGolf(m.input.Value()), m.spinner.Tick)
+			}
+			//m.processing = true
+			if m.cursor == 0 {
+				m.title = "Gas Golfing\nInput Function Selector\n"
+				m.interactive = true
+				return m, tea.Batch(m.input.Focus(), textinput.Blink)
+			} else {
+				return m, tea.Batch(createMessage("Print Message", 2), m.spinner.Tick)
 			}
 		}
 	case printMessage:
@@ -82,39 +89,50 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		return m, cmd
 	case gasGolfResult:
 		m.processing = false
-		m.message = fmt.Sprintf("name: %s\nselector: %s\ntime taken: %v\n", msg.name, msg.selector, msg.timeTaken)
+		m.message = fmt.Sprintf("\nname:\t%s\nselector:\t%s\ntime taken:\t%v", msg.name, msg.selector, msg.timeTaken)
 	}
-	return m, nil
+
+	m.input, cmd = m.input.Update(msg)
+	return m, cmd
 }
 
 func (m model) View() string {
-	s := "What would you like to do ?\n\n"
-	for i, choice := range m.choices {
+	var sb strings.Builder
 
+	sb.WriteString(fmt.Sprintf("%s\n", m.title))
+
+	if m.interactive {
+		writeInteractiveLayout(&sb, m)
+	} else {
+		writeMenuLayout(&sb, m)
+	}
+
+	sb.WriteString("\nPress q or ctrl+c to quit.\n")
+
+	return sb.String()
+}
+
+func writeInteractiveLayout(sb *strings.Builder, m model) {
+	sb.WriteString(m.input.View() + "\n")
+
+	if m.processing {
+		sb.WriteString("\n" + m.spinner.View())
+	}
+
+	if m.message != "" {
+		sb.WriteString(fmt.Sprintf("%s\n", m.message))
+	}
+}
+
+func writeMenuLayout(sb *strings.Builder, m model) {
+	sb.WriteString("What would you like to do ?\n\n")
+	for i, choice := range m.choices {
 		cursor := "  "
 		if m.cursor == i {
 			cursor = "->"
 		}
-
-		checked := " "
-		if _, ok := m.selected[i]; ok {
-			checked = "x"
-		}
-
-		s += fmt.Sprintf("%s [%s] %s\n", cursor, checked, choice)
+		sb.WriteString(fmt.Sprintf("%s %s\n", cursor, choice))
 	}
-
-	if m.processing {
-		s += m.spinner.View()
-	}
-
-	if m.message != "" {
-		s += fmt.Sprintf("\n%s\n", m.message)
-	}
-
-	s += "\nPress q or ctrl+c to quit.\n"
-
-	return s
 }
 
 func gasGolf(funcPattern string) tea.Cmd {
